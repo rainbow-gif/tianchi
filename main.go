b3c013390b634d1356d954ec1b35574e8adcfe06
rainbow-gif <56581441+rainbow-gif@users.noreply.github.com>
2021-07-28 20:48:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,54 +1,132 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-basic/uuid"
+	"log"
+	"net/http"
+)
 
 
 func room(c *gin.Context) {
+	json := make(map[string]string) //注意该结构接受的内容
+	err := c.BindJSON(&json)
+	if err != nil {
+		return 
+	}
+	log.Printf("%v",&json)
+	c.String(http.StatusOK, "response is room id string")
+
 
 }
-func enter(w http.ResponseWriter, r *http.Request) {
+func enter(c *gin.Context) {
+	roomid := c.Param("roomid")
+	if roomid == "1" {
+		c.String(http.StatusOK, "Enter the Room")
+	}else {
 
-}func roomLeave(w http.ResponseWriter, r *http.Request) {
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+}
+func roomLeave(c *gin.Context) {
+	roomid := "1";
+	if roomid == "1" {
+		c.String(http.StatusOK, "Enter the Room")
+	}else {
+
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+}
+func roomid(c *gin.Context) {
+	roomid := c.Param("roomid")
+	if roomid == "1" {
+		c.String(http.StatusOK, "response is room name string")
+	}else {
+
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+}
+func users(c *gin.Context) {
+	roomid := c.Param("roomid")
+	allUsers := []string{"fwef","hiuiu"}
+	if roomid == "1" {
+		c.JSON(http.StatusOK, allUsers)
+	}else {
+
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+}
+func roomList(c *gin.Context) {
+	json := make(map[string]int) //注意该结构接受的内容
+	err := c.BindJSON(&json)
+	if err != nil {
+		return
+	}
+	pageIndex := json["pageIndex"]
+	pageSize := json["pageSize"]
+
+	log.Printf("%v",&json)
+	allUsers := []map[string]string{{"name":"roomname","id":"rommid"}}
+	if pageIndex == pageSize {//假装是这样，其实是分页
+		c.JSON(http.StatusOK, allUsers)
+	}else {
+
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+}
+func user(c *gin.Context) {
+	json := make(map[string]int) //注意该结构接受的内容
+	err := c.BindJSON(&json)
+	if err != nil {
+		return
+	}
+
+	username := json["username"]
+	firstName := json["firstName"]
+	lastName := json["lastName"]
+	email := json["email"]
+	password := json["password"]
+	phone := json["phone"]
+	log.Printf("%s",username+firstName+lastName+email+password+phone)
+	c.AbortWithStatus(http.StatusOK)
+}
+func userLogin(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+	log.Printf("%s",username+password)
+
+	c.String(http.StatusOK, uuid.New())
+}
+func username(c *gin.Context) {
+	username  := c.Param("username")
+	c.JSON(http.StatusOK, gin.H{
+		"firstName": "string",
+		"lastName": "string",
+		"email": "string",
+		"phone": "string"
+	})
+}
+func send(c *gin.Context) {
 
 }
-func roomid(w http.ResponseWriter, r *http.Request) {
-
-}
-func users(w http.ResponseWriter, r *http.Request) {
-
-}
-func roomList(w http.ResponseWriter, r *http.Request) {
-
-}func user(w http.ResponseWriter, r *http.Request) {
-
-}
-func userLogin(w http.ResponseWriter, r *http.Request) {
-
-}
-func username(w http.ResponseWriter, r *http.Request) {
-
-}func send(w http.ResponseWriter, r *http.Request) {
-
-}
-func retrieve(w http.ResponseWriter, r *http.Request) {
+func retrieve(c *gin.Context) {
 
 }
 func main() {
-	
-	
-  r := gin.Default()
+
+	r := gin.Default()
 	r.POST("/room", room)
 	r.PUT("/room/:roomid/enter", enter)
 	r.PUT("/roomLeave", roomLeave)
-  r.GET("/room/:roomid", roomid)
-  r.GET("/room/:roomid/users", users)
+	r.GET("/room/:roomid", roomid)
+	r.GET("/room/:roomid/users", users)
 	r.POST("/roomList", roomList)
-  r.POST("/user", user)
+	r.POST("/user", user)
 	r.GET("/userLogin", userLogin)
-  r.GET("/user/:sername", username)
-  r.POST("/message/send", send)
+	r.GET("/user/:sername", username)
+	r.POST("/message/send", send)
 	r.POST("/message/retrieve", retrieve)
-	http.HandleFunc("/", home)
+	//http.HandleFunc("/", home)
 	r.Run()
-// 默认监听本地8080端口，如果需要更改可以使用 r.Run(":9000")
+	// 默认监听本地8080端口，如果需要更改可以使用 r.Run(":9000")
 }
