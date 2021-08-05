72847ca1e6b1d480f34cb41baa635b6df07001a2
rainbow-gif <56581441+rainbow-gif@users.noreply.github.com>
2021-08-05 10:45:00 +0800

    Update room.sql

--- room.sql
+++ room.sql
@@ -1,5 +1,22 @@
 CREATE TABLE IF NOT EXISTS `room`(
-   `id` INT UNSIGNED AUTO_INCREMENT,
-   `name` VARCHAR(100) UNIQUE NOT NULL,
-    PRIMARY KEY ( `id` )
+                                     `id` INT UNSIGNED AUTO_INCREMENT,
+                                     `name` VARCHAR(100) UNIQUE NOT NULL,
+                                     PRIMARY KEY ( `id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `user`(
+                                     `id` INT UNSIGNED AUTO_INCREMENT,
+                                     `username` VARCHAR(100) UNIQUE NOT NULL,
+                                     `fristname` VARCHAR(100) NOT NULL,
+                                     `lastname` VARCHAR(100) NOT NULL,
+                                     `email` VARCHAR(100) UNIQUE NOT NULL,
+                                     `password` VARCHAR(100) NOT NULL,
+                                     `phone` VARCHAR(20) UNIQUE NOT NULL,
+                                     `roomid` INT UNSIGNED,
+                                     PRIMARY KEY ( `id` ),
+                                     FOREIGN KEY (`roomid`) REFERENCES `room` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `time`(
+                                     `id` VARCHAR(100) NOT NULL,
+                                     `text` VARCHAR(100) NOT NULL,
+                                     `time` INT UNSIGNED
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
